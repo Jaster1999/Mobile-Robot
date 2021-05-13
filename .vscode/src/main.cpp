4e93a8b557fad3d00dfc2b33028da545e20912e9
Jacko308 <73738729+Jacko308@users.noreply.github.com>
2021-05-13 23:23:24 +1200

    ESP32 PC bluetooth serial master confirmed

---
+++ .vscode/src/main.cpp
@@ -0,0 +1,54 @@
+// Techno-weed ESP32 PC bluetooth serial interface
+// 
+#include <Arduino.h>
+#include "BluetoothSerial.h"
+#include "esp_bt_device.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+String MACadd = "AA:BB:CC:11:22:33";
+uint8_t address[6]  = {0x94, 0xB9, 0x7E, 0x7B, 0xA7, 0xE2};
+String name = "TechnoWeed_ESP32test";
+const char *pin = "1234"; //<- standard pin would be provided by default
+bool connected;
+
+//-------------- definitions -------------//
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(9600);
+  //SerialBT.setPin(pin);
+  SerialBT.begin("TW_ESP32PC", true); 
+  //SerialBT.setPin(pin);
+  Serial.println("The device started in master mode, make sure remote BT device is on!");
+  //connected = SerialBT.connect(name);
+  connected = SerialBT.connect(address);
+  
+  if(connected) {
+    Serial.println("Connected Succesfully!");
+  } 
+  else {
+    while(!SerialBT.connected(10000)) {
+      Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app."); 
+    }
+  }
+  if (SerialBT.disconnect()) {
+    Serial.println("Disconnected Succesfully!");
+  }
+  SerialBT.connect();
+
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+
+  if (SerialBT.available()){
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+}
